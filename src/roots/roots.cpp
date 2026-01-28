dd51a9681f5b5c6d1bb612c4a464a8d2fa790d0e
polyominos <ddt.danieltran@gmail.com>
2026-01-28 10:33:26 -0700

    removed redundancy

--- src/roots/roots.cpp
+++ src/roots/roots.cpp
@@ -1,31 +1,4 @@
 #include "roots.hpp"
-
-bool bisection(std::function<double(double)> f,
-               double a, double b,
-               double *root) {
-    return false;
-}
-
-bool regula_falsi(std::function<double(double)> f,
-                  double a, double b,
-                  double *root) {
-    return false;
-}
-
-bool newton_raphson(std::function<double(double)> f,
-                    std::function<double(double)> g,
-                    double a, double b, double c,
-                    double *root) {
-    return false;
-}
-
-bool secant(std::function<double(double)> f,
-            double a, double b, double c,
-            double *root) {
-    return false;
-}
-
-#include "roots.hpp"
 #include <cmath>
 #include <functional>
 #include <limits>
